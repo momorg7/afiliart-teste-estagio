225a94e80d00378ed897d724d1ee6b164eeb7078
momorg7 <morganathalita8@outlook.com>
2020-03-05 20:37:25 -0300

    CRUD quase finalizado.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,8 +2,6 @@ const User = require('../models/User');
 
 module.exports = {
     async create(req, res){
-        console.log(req.body);
-
         const user = {
             email: req.body.email,
             name: req.body.name,
@@ -23,15 +21,41 @@ module.exports = {
         });
     },
 
-    async read(req, res){
+    async index(req, res){
+        users = await User.find({});
+        res.json(users);
+    },
 
+    async show(req, res){
+        User.findById(req.params.id, (err, user)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.json(user);
+            }
+        });
     },
 
     async update(req, res){
-
+        User.findByIdAndUpdate(req.params.id, req.body, { new: true, useFindAndModify: false }, (err, user)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.json(user);
+            }
+        });
     },
 
-    async delete(req, res){
-
+    async destroy(req, res){
+        User.findByIdAndDelete(req.params.id, { useFindAndModify: false }, (err)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.end();
+            }
+        });
     }
 }
--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const UsersController = require('../controllers/UsersController');
 
-router.post('/create', UsersController.create);
+router.post('/', UsersController.create);
+router.get('/', UsersController.index);
+router.get('/:id', UsersController.show);
+router.put('/:id', UsersController.update);
+router.delete('/:id', UsersController.destroy);
 
 module.exports = router;
